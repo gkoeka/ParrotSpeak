b2ab707180863fa858bce1136797838b3d544354
Greg Koeka <42160607-gregkoeka@users.noreply.replit.com>
2025-07-16 18:59:39 +0000

    fix: add @shared alias to webpack config for mobile web build

--- mobile-app/webpack.config.js
+++ mobile-app/webpack.config.js
@@ -1,12 +1,14 @@
+const path = require('path');
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
-  
-  // Customize the config before returning it.
+
+  // Add custom aliases here
   config.resolve.alias = {
     ...config.resolve.alias,
     'react-native$': 'react-native-web',
+    '@shared': path.resolve(__dirname, '../shared'), // <-- This line enables @shared alias
   };
 
   return config;
