4977cda38281497ba77975b272da0b70781e83f4
Greg Koeka <42160607-gregkoeka@users.noreply.replit.com>
2025-09-01 03:54:42 +0000

    Wire Google Sign-In (upload vs play client IDs)updates second

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // required for OAuth redirect
   scheme: "parrotspeak",
-
+  runtimeVersion: { policy: "appVersion" },
   userInterfaceStyle: "light",
   splash: {
     image: "./assets/splash.png",
